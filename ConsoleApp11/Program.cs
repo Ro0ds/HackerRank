7dfe7b4a2748b9c5eb18562bb0c5f279b032ae52
Digo <digo_gabriel97@yahoo.com.br>
2022-05-09 15:43:36 -0300

    HackerRank - SortMaxMin

--- ConsoleApp11/Program.cs
+++ ConsoleApp11/Program.cs
@@ -6,52 +6,28 @@ using System;
 
 namespace ConsoleApp11 {
     class Program {
-        public List<int> arr = new List<int>(); // { 1, 1, 0, -1, -1 }
-        public static Tuple<double, double, double> tuple;
         public static StringBuilder sb = new StringBuilder();
+        public static Tuple<Int64, Int64> tuple;
 
-        static void Main(string[] args) {
-            int n = Convert.ToInt32(Console.ReadLine().Trim());
+        public static void miniMaxSum(List<int> arr) {
+            Int64 resMax = 0;
+            Int64 resMin = 0;
+            arr.Sort();
 
-            List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
-
-            plusMinus(arr);
-
-            sb.AppendLine(tuple.Item1.ToString("F6"));
-            sb.AppendLine(tuple.Item2.ToString("F6"));
-            sb.AppendLine(tuple.Item3.ToString("F6"));
-
-            Console.WriteLine(sb);
-        }
-
-        public static void plusMinus(List<int> arr) {
-            int div = arr.Count;
-            List<double> pos, neg, zer;
-
-
-            pos = new List<double>();
-            neg = new List<double>();
-            zer = new List<double>();
-
-            foreach (double elem in arr) {
-                Convert.ToDouble(elem);
-
-                if (elem > 0) {
-                    pos.Add(elem);
-                }
-                else if (elem < 0) {
-                    neg.Add(elem);
-                }
-                else if (elem == 0) {
-                    zer.Add(elem);
-                }
+            for (int i = 1; i <= 4; i++) {
+                resMax += arr[i];
             }
 
-            tuple = new Tuple<double, double, double>
-                ((Convert.ToDouble(pos.Count) / div),
-                (Convert.ToDouble(neg.Count) / div),
-                (Convert.ToDouble(zer.Count) / div));
+            for (int i = 0; i < 4; i++) {
+                resMin += arr[i];
+            }
+            tuple = new Tuple<long, long>(resMin, resMax);
+        }
+
+        static void Main(string[] args) {
+            miniMaxSum(new List<int> {5,2,3,1,4});
+            sb.AppendLine(tuple.Item1 + " " + tuple.Item2);
+            Console.WriteLine(sb);
         }
     }
-
 }
