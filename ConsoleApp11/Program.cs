14d1f41046f16b26683f26f4eaff5de588c06d94
Digo <digo_gabriel97@yahoo.com.br>
2022-05-10 16:46:05 -0300

    HackerRank - Breaking the Records

--- ConsoleApp11/Program.cs
+++ ConsoleApp11/Program.cs
@@ -6,43 +6,41 @@ using System;
 
 namespace ConsoleApp11 {
     class Program {
-        public static string newTime, oldValue, newValue;
+        public static int maxScore, minScore, maxCount, minCount;
+        public static StringBuilder sb = new StringBuilder();
+        public static List<int> result;
+        public static Tuple<int, int> tuple;
 
-        public static string timeConversion(string s) {
-            if (s.Contains("AM")) {
-                newTime = s.Remove(8, 2);
-                oldValue = s.Remove(2);
-                if(int.Parse(oldValue) > 10) {
-                    newValue = (int.Parse(oldValue) - 12).ToString();
-                }
-                else {
-                    newValue = oldValue;
-                }
-                
-                if(int.Parse(newValue) < 10 && newValue.Length == 1) {
-                    newValue = "0" + newValue;
-                }
+        public static List<int> breakingRecords(List<int> scores) {
+            maxScore = scores[0];
+            minScore = scores[0];
+            result = new List<int>(2);
 
-                newTime = newTime.Replace($"{oldValue}", $"{newValue}");
-            }
-            else if (s.Contains("PM")) {
-                newTime = s.Remove(8, 2);
-                oldValue = s.Remove(2);
-                if(oldValue != "12") {
-                    newValue = (int.Parse(oldValue) + 12).ToString();
+            for (int i = 1; i < scores.Count; i++) {
+                if(scores[i] > maxScore) {
+                    maxScore = scores[i];
+                    maxCount++;
                 }
-                else {
-                    newValue = oldValue;
+                else if(scores[i] < minScore) {
+                    minScore = scores[i];
+                    minCount++;
                 }
-                
-                newTime = newTime.Replace($"{oldValue}", $"{newValue}");
             }
 
-            return newTime;
+            result.Add(maxCount);
+            result.Add(minCount);
+
+            tuple = new Tuple<int, int>(result[0], result[1]);
+
+            return result;
         }
 
         static void Main(string[] args) {
-            Console.WriteLine(timeConversion("12:45:54PM"));
+            Console.WriteLine(breakingRecords(new List<int> { 12, 24, 10, 24 }));
+
+            sb.Append(Program.tuple.Item1 + " " + Program.tuple.Item2);
+
+            Console.WriteLine(sb);
         }
     }
 }
