620b32b3c424892807d25d0606e248b334627808
Digo <digo_gabriel97@yahoo.com.br>
2022-05-19 10:48:44 -0300

    HackerRank - Grading Students

--- ConsoleApp11/Program.cs
+++ ConsoleApp11/Program.cs
@@ -7,20 +7,54 @@ namespace ConsoleApp11 {
     class Program {
         public static StringBuilder sb = new StringBuilder();
         public static void Main(string[] args) {
-            lonelyinteger(new List<int> { 1,2,3,4,3,2,1 });
+            gradingStudents(new List<int> { 4,73,67,38,33 });
+
             Console.WriteLine(sb);
         }
 
-        public static int lonelyinteger(List<int> a) {
-            var result = a.GroupBy(x => x).Where(g => g.Count() < 2).Select(y => y.Key).ToList();
-            int final = 0;
+        public static List<int> gradingStudents(List<int> grades) {
+            List<int> res = new List<int>();
+            int oldGrade = 0;
+            int multGrade = 0;
+            int roundedGrade = 0;
 
-            foreach(var item in result) {
-                final = item;
+            for (int i = 1; i < grades.Count; i++) {
+                if(grades[i] < 35) {
+                    res.Add(grades[i]);
+                }
+                
+                if(grades[i] > 35) {
+                    oldGrade = grades[i];
+                    multGrade = oldGrade;
+
+                    while(multGrade % 5 != 0) {
+                        multGrade++;
+                    }
+
+                    if(multGrade - oldGrade < 3) {
+                        roundedGrade = multGrade;
+                    }
+                    else {
+                        roundedGrade = oldGrade;
+                    }
+
+                    res.Add(roundedGrade);
+                }
             }
 
-            sb.Append(final);
-            return final;
+            int[] final = new int[res.Count];
+
+            for(int i = 0; i < res.Count; i++) {
+                //foreach(int a in res) {
+                    final[i] = res[i];
+                //}
+            }
+
+            foreach (var item in final) {
+                sb.AppendLine($"{item}");
+            }
+
+            return final.ToList();
         }
     }
 }
