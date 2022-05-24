06aafd660940f6baec17a96bd7aa9f133685d270
Digo <digo_gabriel97@yahoo.com.br>
2022-05-24 10:30:02 -0300

    HackerRank - Counting Sort 1

--- ConsoleApp11/Program.cs
+++ ConsoleApp11/Program.cs
@@ -1,54 +1,25 @@
-﻿using System.Collections.Generic;
-using System;
+﻿using System;
 using System.Text;
 using System.Linq;
+using System.Collections.Generic;
 
 namespace ConsoleApp11 {
     class Program {
         public static StringBuilder sb = new StringBuilder();
-        public static int firstDiagSum = 0;
-        public static int secondDiagSum = 0;
-        public static int difference = 0;
 
         public static void Main(string[] args) {
-            Console.Write("Linhas: ");
-            int n = Convert.ToInt32(Console.ReadLine().Trim());
-
-            List<List<int>> arr = new List<List<int>>();
-
-            for (int i = 0; i < n; i++) {
-                Console.WriteLine($"\nNúmero de itens: {n}");
-                Console.Write($"Item #{i}: ");
-                arr.Add(Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList());
-            }
-
-            int result = diagonalDifference(arr);
-            Console.WriteLine(sb);
+            countingSort(new List<int> { 1, 1, 3, 2, 1 });
         }
 
-        public static int diagonalDifference(List<List<int>> arr) {
-            for (int i = 0; i < arr.Count; i++) {
-                for (int j = 0; j < arr.Count; j++) {
-                    if (arr.IndexOf(arr[i]) == arr.IndexOf(arr[j])) {
-                        firstDiagSum += arr[i][j];
-                    }
-                }
+        public static List<int> countingSort(List<int> arr) {
+            int[] range = new int[100];
+
+            foreach (int item in arr) {
+                range[item]++;
             }
 
-            arr.Reverse();
-
-            for (int i = 0; i < arr.Count; i++) {
-                for (int j = 0; j < arr.Count; j++) {
-                    if (arr.IndexOf(arr[i]) == arr.IndexOf(arr[j])) {
-                        secondDiagSum += arr[i][j];
-                    }
-                }
-            }
-
-            difference = firstDiagSum - secondDiagSum;
-            sb.AppendLine($"\ndifference: {difference}");
-
-            return difference;
+            return range.ToList();
         }
+
     }
 }
